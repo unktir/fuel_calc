c55f7f486d156a8f04c9d5de3273d3f641e2c8f1
unktir <valera47304@gmail.com>
2025-02-05 02:38:32 +0500

    chore(stylelint): add the stylelint, sass, eslint-prettier-config

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -3,6 +3,7 @@ import ts from 'typescript-eslint';
 import eslintPluginReact from 'eslint-plugin-react';
 import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
 // TODO: Install eslint-plugin-react-hooks when https://github.com/facebook/react/issues/28313 is resolved
+import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default ts.config(
@@ -11,6 +12,7 @@ export default ts.config(
   eslintPluginReact.configs.flat.recommended,
   eslintPluginReact.configs.flat['jsx-runtime'],
   eslintPluginJsxA11y.flatConfigs.recommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,3 +1,5 @@
+import 'shared/ui/assets/styles';
+
 export function App() {
   return (
     <>
