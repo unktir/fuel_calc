3d5fd78ee62cfdd79a2b62dfcac64e0939e7b8e1
ogsneaky <saiyanboi9@gmail.co>
2025-02-26 23:10:01 +0300

    added cors settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
+CORS_ALLOW_CREDENTIALS = True
