20a5b0f92b6bcc279f8d6dc4ddffecfbacb255df
ogsneaky <saiyanboi9@gmail.co>
2025-02-26 23:35:01 +0300

    changed admin

--- backend/fuel_calc/admin.py
+++ backend/fuel_calc/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import User, Car
 
 admin.site.register(User)
-admin.site.register(Car)
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('name', 'fuel_efficiency', 'fuel_tank_capacity', 'image')
+    list_display = ('name', 'fuel_efficiency', 'tank_capacity', 'image')
     search_fields = ('name',)
