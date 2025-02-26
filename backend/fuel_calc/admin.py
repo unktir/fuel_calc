8829489da3d82b03f7220551f216a02a1d87fcad
ogsneaky <saiyanboi9@gmail.co>
2025-02-26 23:31:48 +0300

    changed settings

--- backend/fuel_calc/admin.py
+++ backend/fuel_calc/admin.py
@@ -3,3 +3,8 @@ from .models import User, Car
 
 admin.site.register(User)
 admin.site.register(Car)
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'fuel_efficiency', 'fuel_tank_capacity', 'image')
+    search_fields = ('name',)
